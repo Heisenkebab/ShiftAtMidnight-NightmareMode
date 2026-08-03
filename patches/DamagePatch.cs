19fb138c46c8ec9ade64501bc1893626438aba0a
Heisenkebab <heisenkebab@proton.me>
2026-08-03 19:55:19 +0200

    refactor: remove unnecessary log

--- patches/DamagePatch.cs
+++ patches/DamagePatch.cs
@@ -4,13 +4,12 @@ using NIGHTMAREMODE;
 [HarmonyPatch(typeof(PlayerManager), "TakeDamage")]
 public static class PlayerDamagePatch
 {
-    static void Prefix(ref float damage, ref bool significantAnim, ref string type)
+    static void Prefix(ref float damage, ref bool significantAnim, ref string type, PlayerManager __instance)
     {
         if (!NightmareSettings.Enabled.Value)
         {
             return;
         }
-        damage *= NightmareSettings.DamageScaling.Value;
-        Plugin.Log.LogInfo($"Scaled damage to {NightmareSettings.DamageScaling.Value}x");
+        damage *= NightmareSettings.DamageTakenScaling.Value;
     }
 }
