bf35841daf9f8b53172299c8837103af6d2cf98a
Heisenkebab <heisenkebab@proton.me>
2026-08-28 16:41:30 +0200

    fix(damage): change method from TakeDamage to Rpc_TakeDamage for scaling on host only

--- Patches/DamagePatch.cs
+++ Patches/DamagePatch.cs
@@ -3,25 +3,23 @@ using HarmonyLib;
 
 namespace NIGHTMAREMODE.Patches;
 
-[HarmonyPatch(typeof(PlayerManager), "TakeDamage")]
+[HarmonyPatch(typeof(PlayerManager), "Rpc_TakeDamage")]
 public static class PlayerDamagePatch
 {
-    static void Prefix(ref float damage, ref bool significantAnim, ref string type, PlayerManager __instance)
+    static void Prefix(ref float damage, PlayerManager __instance)
     {
         if (!NightmareSettings.Enabled.Value) return;
+        if (__instance.Object == null || !__instance.Object.HasStateAuthority) return;
 
         float damageBefore = damage;
+        float damageScale = NightmareSettings.DamageTakenScaling.Value;
+
         HuntManager huntManager = HuntManager.Instance;
         if (huntManager != null && huntManager.huntInProgress)
-        {
-            float damageScale = NightmareSettings.DamageTakenScaling.Value + (0.25f * Math.Max(CompleteTransactionPatch.amountOfDoppelgangerLetThrough - 1, 0));
-            damage *= damageScale;
-            Plugin.Log.LogInfo($"[TakeDamage] Damage Scaled by {damageScale}x ({damageBefore} to {damage})");
-            return;
-        }
+            damageScale += 0.25f * Math.Max(CompleteTransactionPatch.amountOfDoppelgangerLetThrough - 1, 0);
 
-        damage *= NightmareSettings.DamageTakenScaling.Value;
+        damage *= damageScale;
 
-        Plugin.Log.LogInfo($"[TakeDamage] Damage Scaled by {NightmareSettings.DamageTakenScaling.Value}x ({damageBefore} to {damage})");
+        Plugin.Log.LogInfo($"[Rpc_TakeDamage] Damage Scaled by {damageScale}x ({damageBefore} to {damage})");
     }
 }
