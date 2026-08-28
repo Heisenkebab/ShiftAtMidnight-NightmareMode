bf9a6528e41687bc08718b8b5d539a63699ab64e
Heisenkebab <heisenkebab@proton.me>
2026-08-28 16:55:12 +0200

    feat(spider): scale spider health per doppelganger and player

--- Patches/SpiderHealthPatch.cs
+++ Patches/SpiderHealthPatch.cs
@@ -9,12 +9,16 @@ public static class SpiderHealthPatch
     static void Prefix(Spider __instance)
     {
         if (!NightmareSettings.Enabled.Value) return;
-        if (!__instance.Object.HasStateAuthority) return;
+        if (__instance.Object == null || !__instance.Object.HasStateAuthority) return;
+
+        int playerCount = __instance.Object.Runner.SessionInfo.PlayerCount;
 
         float healthBefore = __instance.hittable.health;
-        float health = NightmareSettings.SpiderHealthScaling.Value + (0.2f * Math.Max(CompleteTransactionPatch.amountOfDoppelgangerLetThrough - 1, 0));
+        float health = NightmareSettings.SpiderHealthScaling.Value + (0.2f * Math.Max(CompleteTransactionPatch.amountOfDoppelgangerLetThrough - 1, 0) * playerCount);
+
         __instance.hittable.health *= health;
         __instance.hittable.maxHealth *= health;
-        Plugin.Log.LogInfo($"Scaled enemy health to {health}x ({healthBefore} to {__instance.hittable.health})");
+
+        Plugin.Log.LogInfo($"[Spider] Scaled enemy health to {health}x ({healthBefore} to {__instance.hittable.health})");
     }
 }
